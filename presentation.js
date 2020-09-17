c7967ec7cae8dbfca7c2cb6e4ffdfffacd5dc6bb
ElodieWDJ <56838354+ElodieWDJ@users.noreply.github.com>
2020-09-17 15:42:22 +0200

    Lister client Json

--- presentation.js
+++ presentation.js
@@ -1,17 +1,21 @@
+var service = require("./service");
+// récupération du module `readline`
+var readline = require('readline');
+
 function start(){
 console.log("1.Lister les clients \n99. Sortir")
 }
-exports.start = start;
-// récupération du module `readline`
-var readline = require('readline');
+
+
+
 // création d'un objet `rl` permettant de récupérer la saisie utilisateur
-var rl = readline.createInterface({
+var rep = readline.createInterface({
     input: process.stdin,
     output: process.stdout
     });
 
 // récupération de la saisie utilisateur 
-rl.question('Veuillez saisir un nombre : ', function(saisie) {
+rep.question('Veuillez saisir un nombre : ', function(saisie) {
 // if(rl==1){
 //     console.log(">> Liste des clients");
 // }else if(rl==99){
@@ -23,13 +27,27 @@ rl.question('Veuillez saisir un nombre : ', function(saisie) {
 // rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
 switch(saisie){
     case "1":
-        console.log("1.Lister les clients \n99. Sortir");
+        service.listerClients(function(resultat){
+            if(resultat.error){
+                console.log('oops...')
+            }else {
+                console.log(resultat.data);
+            }
+            start();
+           
+        });
+        
         break;
-    case "99":
+
+        case "2":
+            break;
+        case "99":
         console.log("Aurevoir");
+        rep.close();
+        this.process.exit(); // Met fin au programme
         break;
 
 }
 
  });
-
+ exports.start = start;
